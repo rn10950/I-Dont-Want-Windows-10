0382332a9cc1950484dee93b9a26a6ce901efc1d
Ryan Nematz <rn10950@gmail.com>
2015-06-06 23:37:34 -0400

    Windows 8 Manifests

--- I Don't Want Windows 10/IDontWantWindows10_i.c
+++ I Don't Want Windows 10/IDontWantWindows10_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 6.00.0366 */
-/* at Mon Jun 01 22:18:08 2015
+/* at Sat Jun 06 22:16:41 2015
  */
 /* Compiler settings for .\IDontWantWindows10.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
--- I Don't Want Windows 10/resource.h
+++ I Don't Want Windows 10/resource.h
@@ -7,12 +7,13 @@
 #define IDR_MAINFRAME                   128
 #define IDR_POWERSHELL1                 129
 #define IDB_BITMAP1                     131
+#define IDR_RT_MANIFEST1                136
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
